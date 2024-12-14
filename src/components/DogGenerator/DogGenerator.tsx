e89bc7872c1c7c399edb621ed876e76e9c56b75f
charlehs <charlehs@gmail.com>
2024-12-14 14:47:01 +0000

    we hooked up the api and made some style changes for the image

--- src/components/DogGenerator/DogGenerator.tsx
+++ src/components/DogGenerator/DogGenerator.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Button } from "../Button/Button";
 import styles from "./DogGenerator.module.scss";
 const DogGenerator = () => {
+  const [imageUrl, setImageUrl] = useState("");
+  const getDogImage = async () => {
+    const url = "https://dog.ceo/api/breeds/image/random";
+    try {
+      const response = await fetch(url);
+      if (!response.ok)
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      const data = await response.json();
+      console.log(data, "data");
+      setImageUrl(data.message);
+      return data;
+    } catch (error: any) {
+      console.error("There was an error fetching the data:", error);
+    }
+  };
+
   return (
     <div className={styles.background}>
       <div className={styles.layout}>
-        <div className={styles.image}>
-          <img />
+        <div className={styles.imageContainer}>
+          {imageUrl && (
+            <img
+              className={styles.image}
+              src={imageUrl}
+              alt="An image of a dog"
+            />
+          )}
         </div>
-        <Button className={styles.generateButton}>Generate</Button>
+        <Button onClick={getDogImage} className={styles.generateButton}>
+          Generate
+        </Button>
       </div>
     </div>
   );
